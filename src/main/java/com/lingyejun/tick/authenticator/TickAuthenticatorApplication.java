e9cd0d24aa396a16dc032a92e56cf04697205349
lilingye <lilingyejun@gmail.com>
2018-11-14 22:49:13 +0800

    add comment

--- src/main/java/com/lingyejun/tick/authenticator/TickAuthenticatorApplication.java
+++ src/main/java/com/lingyejun/tick/authenticator/TickAuthenticatorApplication.java
@@ -1,5 +1,11 @@
 package com.lingyejun.tick.authenticator;
 
+/**
+ * @Author: Lingye
+ * @Date: 2018/11/14
+ * @Describe: Authenticator Application
+ * @Modified By:
+ */
 public class TickAuthenticatorApplication {
 
     public static void main(String[] args) {
