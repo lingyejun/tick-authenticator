ba113060d9c4df755a2b2ddcfd60159ce6334cb6
lilingye <lilingyejun@gmail.com>
2019-02-18 23:54:45 +0800

    add ITickAuthenticator

---
+++ src/main/java/com/lingyejun/authenticator/tick/ITickAuthenticator.java
@@ -0,0 +1,10 @@
+package com.lingyejun.authenticator.tick;
+
+/**
+ * @Author: lingyejun
+ * @Date: 2019/2/18
+ * @Describe: TickAuthenticator Interface
+ * @Modified By:
+ */
+public interface ITickAuthenticator {
+}
