9d87539eed3450e2c28bab25bd1af8381c5353fc
lilingye <lilingyejun@gmail.com>
2019-02-21 22:47:46 +0800

    add Hmac types

--- src/main/java/com/lingyejun/authenticator/tick/HmacTypeEnum.java
+++ src/main/java/com/lingyejun/authenticator/tick/HmacTypeEnum.java
@@ -7,4 +7,19 @@ package com.lingyejun.authenticator.tick;
  * @Modified By:
  */
 public enum HmacTypeEnum {
+
+    HmacSHA1("SHA1"),
+    HmacSHA256("SHA256"),
+    HmacSHA512("SHA512");
+
+    private String hmacType;
+
+    HmacTypeEnum(String hmacType) {
+        this.hmacType = hmacType;
+    }
+
+    public String getHmacType() {
+        return hmacType;
+    }
+
 }
