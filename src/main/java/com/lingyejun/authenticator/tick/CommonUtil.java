65491eb2088f4681f6f267ac8204f50ca02356a3
lilingye <lilingyejun@gmail.com>
2019-02-27 22:27:14 +0800

    add hexStr2Bytes

--- src/main/java/com/lingyejun/authenticator/tick/CommonUtil.java
+++ src/main/java/com/lingyejun/authenticator/tick/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.lingyejun.authenticator.tick;
 
+import java.math.BigInteger;
+
 /**
  * 工具类
  *
@@ -28,9 +30,33 @@ public class CommonUtil {
         return buffer.toString();
     }
 
+    /**
+     * 十六进制字符串转字节数组
+     *
+     * @param hex
+     * @return
+     */
+    public static byte[] hexStr2Bytes(String hex){
+
+        // Adding one byte to get the right conversion
+        // values starting with "0" can be converted
+        byte[] bArray = new BigInteger("10" + hex,16).toByteArray();
+
+        // Copy all the REAL bytes, not the "first"
+        byte[] ret = new byte[bArray.length - 1];
+        for (int i = 0; i < ret.length ; i++)
+            ret[i] = bArray[i+1];
+        return ret;
+    }
+
     public static void main(String[] args) {
-        byte[] bytes = new byte[]{34 ,112, -98, 94, 34, -56, 0, 8, -21, 11, 35, -73, -18, -111, 80, -74, 31, 119, -11, -34};
-        System.out.println(bytesToHexString(bytes));
+        byte[] bytes = new byte[]{34, 112, -98, 94, 34, -56, 0, 8, -21, 11, 35, -73, -18, -111, 80, -74, 31, 119, -11, -34};
+        String hex = bytesToHexString(bytes);
+        System.out.println(hex);
+        byte[] newByte = hexStr2Bytes(hex);
+        for (int i = 0; i < newByte.length; i++) {
+            System.out.print(newByte[i]+" ");
+        }
     }
 
 
